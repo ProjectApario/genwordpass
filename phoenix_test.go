e7f88e0d6389b98b95d75361ccda4823957b8878
Andrei Merlescu <andrei+github@merlescu.net>
2025-06-11 15:51:08 -0400

    Added Makefile

--- phoenix_test.go
+++ phoenix_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// BenchmarkPhoenix determines the ns/op per NewPassword() invocation
 func BenchmarkPhoenix(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		i, e := NewPhoenix()
@@ -16,6 +17,77 @@ func BenchmarkPhoenix(b *testing.B) {
 	}
 }
 
+// FuzzNewPhoenix will use Go's Fuzz testing functionality to challenge the CLI arguments that can be passed into the app
+func FuzzNewPhoenix(f *testing.F) {
+	originalArgs := os.Args
+	defer func() {
+		os.Args = originalArgs
+	}()
+
+	testCases := []struct {
+		languages   string
+		words       string
+		separators  string
+		expectError bool
+		description string
+	}{
+		{"", "3", "1", false, "empty languages should use defaults"},
+		{"en", "3", "1", false, "valid case"},
+		{"en,es,fr", "5", "2", false, "multiple languages"},
+		{"invalid", "3", "1", false, "invalid language should not fail parsing"},
+		{"en", "-1", "1", true, "negative words should fail"},
+		{"en", "abc", "1", true, "non-numeric words should fail"},
+		{"en", "1000", "1", true, "words too large should fail"},
+		{"", "", "", true, "all empty should fail"},
+		{"en", "3", "abc", true, "non-numeric separators should fail"},
+		{"en,", "3", "1", false, "trailing comma should be handled"},
+		{",en", "3", "1", false, "leading comma should be handled"},
+		{"en,,es", "3", "1", false, "double comma should be handled"},
+		{"EN", "3", "1", false, "uppercase language should work"},
+		{"en-US", "3", "1", false, "language with region should work"},
+		{"123", "3", "1", false, "numeric language should not fail parsing"},
+		{"en", "0", "1", true, "zero words should fail"},
+		{"en", "3", "-999", true, "very negative separators should fail"},
+		{"en", "999999", "1", true, "very large words should fail"},
+		{"a", "3", "1", false, "single char language should not fail parsing"},
+		{"en", "1.5", "1", true, "decimal words should fail"},
+		{"en", "3", "1.5", true, "decimal separators should fail"},
+		{"en", "1", "1", true, "words below minimum (3) should fail"},
+		{"en", "2", "1", true, "words below minimum (3) should fail"},
+		{"en", "70", "1", true, "words above maximum (69) should fail"},
+		{"en", "3", "0", true, "separators below minimum (1) should fail"},
+		{"en", "3", "8", true, "separators above maximum (7) should fail"},
+		{"en", "69", "7", false, "maximum valid values should work"},
+	}
+
+	for _, tc := range testCases {
+		f.Add(tc.languages, tc.words, tc.separators, tc.expectError)
+	}
+
+	f.Fuzz(func(t *testing.T, languages, words, separators string, expectError bool) {
+		os.Args = []string{
+			originalArgs[0],
+			"-languages", languages,
+			"-words", words,
+			"-separators", separators,
+		}
+
+		ima, bootErr := NewPhoenix()
+
+		if expectError {
+			assert.Error(t, bootErr, "Expected error for args: languages=%q, words=%q, separators=%q",
+				languages, words, separators)
+		} else {
+			assert.NoError(t, bootErr, "Expected no error for args: languages=%q, words=%q, separators=%q",
+				languages, words, separators)
+			if bootErr == nil {
+				assert.NotNil(t, ima, "Expected non-nil result when no error occurred")
+			}
+		}
+	})
+}
+
+// TestPhoenix consumes the runtime of the application to verify full coverage of package
 func TestPhoenix(t *testing.T) {
 	langs := []string{"en", "de", "fr", "ro", "es", "ru"}
 	for _, lang := range langs {
